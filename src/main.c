e1d3db485142da9dc52721c657a24d192d3be048
Herson Lozano <hersonx@gmail.com>
2026-05-17 07:18:00 -0600

    update de main.c

--- src/main.c
+++ src/main.c
@@ -1,15 +1,13 @@
 #include "fenster.h"
 
-int main() {
-    // Definimos el ancho y alto
-    const int W = 800;
-    const int H = 800;
-    
-    // Creamos un arreglo de píxeles (nuestro lienzo)
-    uint32_t buf[W * H];
+// 1. Usamos #define para crear constantes reales en C
+#define W 320
+#define H 240
 
+int main() {
+    // 2. Ahora el compilador sabe exactamente que esto es buf[76800]
+    uint32_t buf[W * H];
     
-    // Inicializamos la estructura de Fenster
     struct fenster f = { 
         .title = "Mi primer Fenster con XMake", 
         .width = W, 
@@ -19,18 +17,15 @@ int main() {
     
     fenster_open(&f);
     
-    uint32_t color = 0x000000; // Empezamos en negro
+    uint32_t color = 0x000000;
     
-    // Bucle principal: fenster_loop devuelve 0 mientras no cerremos la ventana
     while (fenster_loop(&f) == 0) {
-        // Pintamos todos los píxeles del lienzo
         for (int i = 0; i < W * H; i++) {
             buf[i] = color;
         }
         
-        color += 0x000005; // Aclaramos el color un poco en cada frame
-        
-        fenster_sleep(16); // Pausamos ~16ms para ir a 60 FPS
+        color += 0x000005;
+        fenster_sleep(16);
     }
     
     fenster_close(&f);
